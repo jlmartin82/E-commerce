515c0ea57a79c2e5957cef616d3fa839e296fa98
Justin <justin.martin@hsc.utah.edu>
2023-08-10 18:17:02 -0600

    updates to routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,11 +4,37 @@ const { Product, Category, Tag, ProductTag } = require("../../models");
 // The `/api/products` endpoint
 
 // get all products
-router.get("/", async (req, res) => {
-  // find all products
+router.get("/:id", async (req, res) => {
+  // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findAll({
+    const productData = await Product.findByPk(req.params.id, {
+      include: [
+        {
+          model: Tag,
+          through: ProductTag,
+        },
+        Category,
+      ],
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: "No product found with this id" });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get one product
+router.get("/:id", async (req, res) => {
+  // find a single product by its `id`
+  // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk({
       include: [
         {
           model: Tag,
@@ -18,26 +44,6 @@ router.get("/", async (req, res) => {
       ],
     });
     res.status(200).json(productData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// get one product
-router.get("/:id", (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
-  try {
-    const productData = async await Product.findByPk(req.params.id, {
-      include: [],
-    });
-
-    if (!productDataData) {
-      res.status(404).json({ message: "No product found with this id" });
-      return;
-    }
-
-    res.status(200).json(categoriesData);
   } catch (err) {
     res.status(500).json(err);
   }
